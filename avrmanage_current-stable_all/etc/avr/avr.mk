39356e11735fc49775c5cba4acdb9566c62e0a91
TheCodingGuy <87890942+itsanirudhsrinivasan@users.noreply.github.com>
2022-08-19 17:18:06 +0530

    Update avr.mk

--- avrmanage_current-stable_all/etc/avr/avr.mk
+++ avrmanage_current-stable_all/etc/avr/avr.mk
@@ -1,7 +1,7 @@
 all: $(file).c
 	mkdir -p build-$(chip-id)
 	avr-gcc -Os -c -mmcu=$(chip) -o build-$(chip-id)/$(file).o $(file).c -DF_CPU=$(freq)
-	avr-gcc -mmcu=atmega328p -o build-$(chip-id)/$(file).elf build-$(chip-id)/$(file).o $(dependency1) $(dependency2)
+	avr-gcc -mmcu=$(chip) -o build-$(chip-id)/$(file).elf build-$(chip-id)/$(file).o $(dependency1) $(dependency2)
 	avr-objcopy -O ihex build-$(chip-id)/$(file).elf build-$(chip-id)/avr.hex
 	avr-size --mcu=$(chip) --format=avr -C build-$(chip-id)/$(file).elf
 	avrdude -c $(programmer) -p $(chip-id) -e -U flash:w:build-$(chip-id)/avr.hex -b $(baudrate)
