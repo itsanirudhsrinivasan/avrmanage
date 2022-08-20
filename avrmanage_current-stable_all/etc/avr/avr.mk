4e833d114e43c740a9f57ca343ba3abc1f1be777
TheCodingGuy <87890942+itsanirudhsrinivasan@users.noreply.github.com>
2022-08-20 09:27:37 +0530

    Update to 2.0

--- avrmanage_current-stable_all/etc/avr/avr.mk
+++ avrmanage_current-stable_all/etc/avr/avr.mk
@@ -4,4 +4,4 @@ all: $(file).c
 	avr-gcc -mmcu=$(chip) -o build-$(chip-id)/$(file).elf build-$(chip-id)/$(file).o $(dependency1) $(dependency2)
 	avr-objcopy -O ihex build-$(chip-id)/$(file).elf build-$(chip-id)/avr.hex
 	avr-size --mcu=$(chip) --format=avr -C build-$(chip-id)/$(file).elf
-	avrdude -c $(programmer) -p $(chip-id) -e -U flash:w:build-$(chip-id)/avr.hex -b $(baudrate)
+	avrdude -c $(programmer) -p $(chip-id) -e -U flash:w:build-$(chip-id)/avr.hex -b $(baudrate) -P $(port)
